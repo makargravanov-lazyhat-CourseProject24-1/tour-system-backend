f7f0934ddcf8a9f1651f289418f466f269de51c9
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 21:44:02 +0300

    add other repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,9 +5,24 @@ rootProject.name = "tour-system-backend"
 val common = "ts-backend-common"
 include(":$common")
 
-val clientwebbff = "ts-client-web-bff"
-include(":$clientwebbff")
+val client_web_bff = "ts-client-web-bff"
+include(":$client_web_bff")
 
-val userservice = "ts-user-service"
-include(":$userservice")
+val user = "ts-user-service"
+include(":$user")
+
+val email = "ts-email-service"
+include(":$email")
+
+val tickets = "ts-tickets-service"
+include(":$tickets")
+
+val tour_processing = "ts-tour-processing-service"
+include(":$tour_processing")
+
+val payment = "ts-payment-service"
+include(":$payment")
+
+val agency = "ts-agency-service"
+include(":$agency")
 
