0bf9a538fd5561574df3d0d4e4340728ce1e86a0
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 21:11:34 +0300

    update service 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,19 @@
 plugins {
 	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.spring.kotlin) apply false
-	alias(libs.plugins.spring.boot) apply false
-	alias(libs.plugins.spring.dependencyManagement) apply false
 }
 
 repositories {
 	mavenCentral()
 }
 
-subprojects {
+allprojects {
 	group = "ru.jetlabs"
 	version = "0.0.1-SNAPSHOT"
 
 	apply { plugin(rootProject.libs.plugins.kotlin.jvm.get().pluginId) }
-	apply { plugin(rootProject.libs.plugins.spring.kotlin.get().pluginId) }
-	apply { plugin(rootProject.libs.plugins.spring.boot.get().pluginId) }
-	apply { plugin(rootProject.libs.plugins.spring.dependencyManagement.get().pluginId) }
 
 	repositories {
 		mavenCentral()
-	}
-
-	dependencies {
-		implementation(rootProject.libs.spring.boot.starter)
-		implementation(rootProject.libs.spring.webMVC)
-		implementation(rootProject.libs.kotlin.reflect)
-		implementation(rootProject.projects.tsBackendCommon)
 	}
 
 	java {
