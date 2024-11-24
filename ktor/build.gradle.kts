5f6e8524eb8d54c43f80782a0d7bee51b94d85d7
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 20:25:43 +0300

    add ktor dependencies

--- ktor/build.gradle.kts
+++ ktor/build.gradle.kts
@@ -1,5 +1,13 @@
+plugins {
+    alias(libs.plugins.ktor) apply false
+}
+
 subprojects {
+    apply { plugin(rootProject.libs.plugins.ktor.get().pluginId) }
+
     dependencies {
         implementation(rootProject.projects.tsBackendCommon)
+        implementation(rootProject.libs.ktor.core)
+        implementation(rootProject.libs.ktor.netty)
     }
 }
