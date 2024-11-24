282ffa15b41cbc86b63bcefa65107bd1a6db7590
lazyhat <sevapetrov2016p@gmail.com>
2024-11-24 14:32:27 +0300

    fix build files 4

--- spring/example/src/main/kotlin/ru/jetlabs/ts/example/TourSystemBackendApplication.kt
+++ spring/example/src/main/kotlin/ru/jetlabs/ts/example/TourSystemBackendApplication.kt
@@ -2,10 +2,13 @@ package ru.jetlabs.ts.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import ru.jetlabs.ts.common.Example
 
 @SpringBootApplication
 class TourSystemBackendApplication
 
 fun main(args: Array<String>) {
 	runApplication<TourSystemBackendApplication>(*args)
+
+	val k = Example() // FROM COMMON MODULE
 }
