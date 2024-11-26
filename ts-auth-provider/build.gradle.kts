be9f95c0af5a3ca267f78a9ba6419e4df7c6d030
lazyhat <sevapetrov2016p@gmail.com>
2024-11-26 19:11:29 +0300

    move dependencies to toml

--- ts-auth-provider/build.gradle.kts
+++ ts-auth-provider/build.gradle.kts
@@ -9,10 +9,10 @@ dependencies {
     implementation(libs.spring.boot.webMVC)
     implementation(libs.kotlin.reflect)
     implementation(projects.tsBackendCommon)
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.projectlombok:lombok")
-    implementation("io.jsonwebtoken:jjwt:0.11.5")
-    testImplementation("org.springframework.security:spring-security-test")
+    implementation(libs.lombok)
+    implementation(libs.spring.boot.security)
+    implementation(libs.spring.boot.securityTest)
+    implementation(libs.jjwt)
 }
 
 java {
