1eb068ed0264a4992957c4e84da8f28956bab15d
lazyhat <sevapetrov2016p@gmail.com>
2024-12-25 17:28:15 +0300

    add ts-tour-data-service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,29 +3,32 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "tour-system-backend"
 
 val common = "ts-backend-common"
-includeBuild("$common")
+includeBuild(common)
 
 val client_web_bff = "ts-client-web-bff"
-includeBuild("$client_web_bff")
+includeBuild(client_web_bff)
 
 val user = "ts-user-service"
-includeBuild("$user")
+includeBuild(user)
 
 val email = "ts-email-service"
-includeBuild("$email")
+includeBuild(email)
 
 val tickets = "ts-tickets-service"
-includeBuild("$tickets")
+includeBuild(tickets)
 
 val tour_processing = "ts-tour-processing-service"
-includeBuild("$tour_processing")
+includeBuild(tour_processing)
 
 val payment = "ts-payment-service"
-includeBuild("$payment")
+includeBuild(payment)
 
 val agency = "ts-agency-service"
-includeBuild("$agency")
+includeBuild(agency)
 
 val auth_provider = "ts-auth-provider"
-includeBuild("$auth_provider")
+includeBuild(auth_provider)
+
+val tour_data = "ts-tour-data-service"
+includeBuild(tour_data)
 
